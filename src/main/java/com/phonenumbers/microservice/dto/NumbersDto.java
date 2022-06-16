a6b25093e584e84ecd0bb50c4d0d43506f6ad064
Кристина <kristofija91@inbox.lv>
2022-06-16 20:34:54 +0300

    commit

--- src/main/java/com/phonenumbers/microservice/dto/NumbersDto.java
+++ src/main/java/com/phonenumbers/microservice/dto/NumbersDto.java
@@ -2,11 +2,9 @@ package com.phonenumbers.microservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class NumbersDto {
     Integer phoneNumbers;
     String countryName;
